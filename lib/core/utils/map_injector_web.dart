ac8dd13e38a85311d9e8446613a64f1c3a8cdaba
jacoboale27 <jacoboguirola@gmail.com>
2026-07-28 16:42:29 -0600

    fix: migrate map injector from dart:html to package:web (modern API)

--- lib/core/utils/map_injector_web.dart
+++ lib/core/utils/map_injector_web.dart
@@ -1,11 +1,12 @@
-import 'dart:html' as html;
+import 'package:web/web.dart' as web;
 
 void injectGoogleMapsScript(String apiKey) {
-  if (html.document.head != null) {
-    final script = html.ScriptElement()
+  final head = web.document.head;
+  if (head != null) {
+    final script = web.HTMLScriptElement()
       ..type = 'text/javascript'
       ..src = 'https://maps.googleapis.com/maps/api/js?key=$apiKey'
       ..async = true;
-    html.document.head!.append(script);
+    head.append(script);
   }
 }
