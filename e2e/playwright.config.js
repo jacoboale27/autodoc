0ae0a5a5a366f915b4d9f6be79a25b146b30ddd0
jacoboale27 <jacoboguirola@gmail.com>
2026-07-31 17:14:37 -0600

    chore(e2e): initialize Playwright configuration and local web server setup
    (cherry picked from commit 951f00c15a580883c6650c32721decaa7a44a95a)

---
+++ e2e/playwright.config.js
@@ -0,0 +1,22 @@
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  timeout: 60000,
+  retries: 0,
+  reporter: [['json', { outputFile: 'results/test-results.json' }], ['html']],
+  use: {
+    baseURL: 'http://localhost:5555',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure'
+  },
+  projects: [
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } }
+  ],
+  webServer: {
+    command: 'cd .. && flutter run -d web-server --web-port=5555 --web-renderer html',
+    port: 5555,
+    timeout: 120 * 1000,
+    reuseExistingServer: true
+  }
+});
