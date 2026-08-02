93d003b6fdf41bd1eda7670830b046b181108a3a
jacoboale27 <jacoboguirola@gmail.com>
2026-08-02 09:08:08 -0600

    feat(chat): add AudioChatCard voice note player

---
+++ lib/features/chat/presentation/widgets/cards/audio_chat_card.dart
@@ -0,0 +1,86 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+import 'package:autodoc/core/theme/app_colors.dart';
+
+class AudioChatCard extends StatefulWidget {
+  final String urlArchivo;
+  final int? duracionSegundos;
+  final bool isMe;
+
+  const AudioChatCard({
+    super.key,
+    required this.urlArchivo,
+    required this.duracionSegundos,
+    required this.isMe,
+  });
+
+  @override
+  State<AudioChatCard> createState() => _AudioChatCardState();
+}
+
+class _AudioChatCardState extends State<AudioChatCard> {
+  final AudioPlayer _player = AudioPlayer();
+  bool _reproduciendo = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _player.onPlayerComplete.listen((_) {
+      if (mounted) setState(() => _reproduciendo = false);
+    });
+  }
+
+  Future<void> _toggle() async {
+    if (_reproduciendo) {
+      await _player.pause();
+    } else {
+      await _player.play(UrlSource(widget.urlArchivo));
+    }
+    if (mounted) setState(() => _reproduciendo = !_reproduciendo);
+  }
+
+  String get _duracionFormateada {
+    final segundos = widget.duracionSegundos ?? 0;
+    final min = segundos ~/ 60;
+    final seg = (segundos % 60).toString().padLeft(2, '0');
+    return '$min:$seg';
+  }
+
+  @override
+  void dispose() {
+    _player.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).extension<AppColors>();
+    final contentColor = widget.isMe
+        ? Colors.white
+        : (colors?.textPrimary ?? Colors.black87);
+
+    return Container(
+      margin: const EdgeInsets.only(top: 8),
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      decoration: BoxDecoration(
+        color: widget.isMe
+            ? (colors?.primary ?? Theme.of(context).colorScheme.primary)
+            : Colors.grey.shade200,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IconButton(
+            icon: Icon(
+              _reproduciendo ? Icons.pause : Icons.play_arrow,
+              color: contentColor,
+            ),
+            onPressed: _toggle,
+          ),
+          Text(_duracionFormateada, style: TextStyle(color: contentColor)),
+        ],
+      ),
+    );
+  }
+}
